947f0eac7cda8469982145a19bf5635ae2bfb3c7
Best Telegram Marketing Bots 2025 <139961701+QQSHILL@users.noreply.github.com>
2025-09-22 15:57:35 +0800

    Add files via upload

---
+++ telegram-forwarding-bot.py
@@ -0,0 +1,14 @@
+"""
+Telegram Forwarding Bot - Information Script
+Created on: Sept 22, 2025
+Author: QQSHILL
+
+"""
+
+def show_info():
+    print("🚀 Welcome to Telegram Forwarding Bot 🚀")
+    print("This bot lets you forward messages to many Telegram group 24/7.")
+    print("🔗 Learn more and get started here: https://www.qqshill.com/telegram-forwarding-bot")
+    
+if __name__ == "__main__":
+    show_info()
